fdb6585796bc83966ee1a4982312082a16506aac
Boss-Master <masterthesis20232024@gmail.com>
2025-02-25 10:53:50 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -30,7 +30,7 @@ const Index = () => {
         {/* About Section */}
 <section id="about" className="about-section py-24">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <div className="bg-gray-50 dark:bg-gray-900 p-8 rounded-xl shadow-lg">
+    <div className="bg-gray-50 dark:bg-gray-800 p-8 rounded-xl shadow-lg">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
         <div>
           <img
